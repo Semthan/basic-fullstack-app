e092033de1416b8a485f8bbb65d930ea0818421c
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-12-21 02:15:31 +0100

    monogoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
 const app = express()
 
-const port = 5000
+const port = process.env.PORT || 5000
+const uri = process.env.ATLAS_URI
 
+mongoose.set('strictQuery', false);
+mongoose.connect(uri, { useNewUrlParser: true });
+const { connection } = mongoose;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+r
 app.use('/', require('./routes/index'))
 app.use('/product', require('./routes/product'))
 
