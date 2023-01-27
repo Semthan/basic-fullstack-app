d71e7e8cfdd81f91cf7b0281bf530f53f98f5e36
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2023-01-27 15:25:30 +0100

    deploy test

--- frontend/src/fetches.js
+++ frontend/src/fetches.js
@@ -2,11 +2,11 @@ import axios from 'axios'
 
 
 const url = axios.create({
-  baseURL: 'http://127.0.0.1:10000'
+  baseURL: '/product'
 })
 
-export const getAllProducts = () => url.get('/product')
-export const getSingleProduct = (id) => url.get(`/product/${id}`)
-export const addProduct = (payload) => url.post('/product', payload)
-export const updateProduct = (payload, id) => url.put(`/product/${id}`, payload)
-export const deleteProduct = (id) => url.delete(`/product/${id}`)
+export const getAllProducts = () => url.get('')
+export const getSingleProduct = (id) => url.get(`/${id}`)
+export const addProduct = (payload) => url.post('', payload)
+export const updateProduct = (payload, id) => url.put(`/${id}`, payload)
+export const deleteProduct = (id) => url.delete(`/${id}`)
