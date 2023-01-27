0c16aece8b15fed19e38305b740cc6b91b19214d
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2023-01-27 01:51:06 +0100

    backend url

--- frontend/src/fetches.js
+++ frontend/src/fetches.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const url = axios.create({
-  baseURL: 'http://127.0.0.1:5000'
+  baseURL: 'https://basic-mern-app.onrender.com'
 })
 
 export const getAllProducts = () => url.get('/product')
