c593d08bdbcb50a10841f781dc7510078241f69e
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2023-02-03 21:13:22 +0100

    backend baseurl

--- frontend/src/fetches.js
+++ frontend/src/fetches.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const url = axios.create({
-  baseURL: 'http://127.0.0.1:5000'
+  baseURL: 'http://basic-mern-app-api'
 })
 
 export const getAllProducts = () => url.get('/api')
