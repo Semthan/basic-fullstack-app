4cd02c88b63a5c2b5e266263ca5ea67577c0cd10
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2023-02-03 20:33:46 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -16,20 +16,18 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
 
-/* app.use(
+app.use(
   cors({
     origin: ['http://localhost:3000',],
     credentials: true,
   }),
-); */
+);
 
-//first lets try with this
-app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', require('./routes/index'))
-app.use('/product', require('./routes/product'))
+app.use('/api', require('./routes/product'))
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
