ab4a916a5a5b50d3741ed3fdf5bc4803668a808c
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2023-02-04 19:54:06 +0100

    changed base url

--- frontend/src/fetches.js
+++ frontend/src/fetches.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 
-const url = axios.create({
+/* const url = axios.create({
   baseURL: 'https://basic-mern-app-api.onrender.com'
-})
+}) */
 
-export const getAllProducts = () => url.get('/api')
-export const getSingleProduct = (id) => url.get(`/api/${id}`)
-export const addProduct = (payload) => url.post('/api', payload)
-export const updateProduct = (payload, id) => url.put(`/api/${id}`, payload)
-export const deleteProduct = (id) => url.delete(`/api/${id}`)
+export const getAllProducts = () => axios.get('/api')
+export const getSingleProduct = (id) => axios.get(`/api/${id}`)
+export const addProduct = (payload) => axios.post('/api', payload)
+export const updateProduct = (payload, id) => axios.put(`/api/${id}`, payload)
+export const deleteProduct = (id) => axios.delete(`/api/${id}`)
